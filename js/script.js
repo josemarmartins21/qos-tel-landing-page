6fe34f311d6c45bc69e8c12a11462f45535aeee8
Josemar <josemar21@outlook.pt>
2026-02-11 11:38:09 +0100

    melhorei a secção de perguntas e respostas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 var ano_atual = document.getElementById('ano')
 var ano = new Date().getFullYear()
 
+var perguntasContainer = document.querySelectorAll('.pergunta-container')
+
+perguntasContainer.forEach((container) => {
+    var mostrar = container.querySelector('#mostrar')
+    var esconder = container.querySelector('#esconder')
+    var resposta = container.querySelector('.resposta')
+
+    if (!mostrar || !esconder || !resposta) return
+
+    mostrar.addEventListener('click', () => {
+        resposta.classList.add('show')
+        esconder.style.display = 'inline-flex'
+        mostrar.style.display = 'none'
+    })
+
+    esconder.addEventListener('click', () => {
+        resposta.classList.remove('show')
+        esconder.style.display = 'none'
+        mostrar.style.display = 'inline-flex'
+    })
+})
+
+
+
 function atualizarAno() {
     ano_atual.innerText = ano 
 }
