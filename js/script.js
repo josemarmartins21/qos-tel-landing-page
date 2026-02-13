cbb83c12f6dfc19b9eb5114f1b5b4c7422a64f57
Josemar <josemar21@outlook.pt>
2026-02-13 06:44:50 +0100

    Trasinção ao a abrir as FAQ adicionada

--- js/script.js
+++ js/script.js
@@ -10,13 +10,17 @@ perguntasContainer.forEach((container) => {
 
     if (!mostrar || !esconder || !resposta) return
 
+    resposta.style.maxHeight = '0px'
+
     mostrar.addEventListener('click', () => {
         resposta.classList.add('show')
+        resposta.style.maxHeight = resposta.scrollHeight + 'px'
         esconder.style.display = 'inline-flex'
         mostrar.style.display = 'none'
     })
 
     esconder.addEventListener('click', () => {
+        resposta.style.maxHeight = '0px'
         resposta.classList.remove('show')
         esconder.style.display = 'none'
         mostrar.style.display = 'inline-flex'
